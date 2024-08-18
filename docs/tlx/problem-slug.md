55331bce5ef8b6510cc23f38eec5e804b40232f1
Ashar Fuadi <fushar@gmail.com>
2024-08-18 17:25:39 +0700

    Prepare for slug doc

---
+++ docs/tlx/problem-slug.md
@@ -0,0 +1,1 @@
+# Panduan Membuat Slug Soal
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ia-toki/panduan/blob/main/',
         },
         blog: false,
         theme: {
@@ -67,7 +67,7 @@ const config = {
         },
         items: [
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/ia-toki/panduan',
             label: 'GitHub',
             position: 'right',
           },
