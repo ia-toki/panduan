5dc559caaf94bf19a49c367a8edee541ba15f87a
Ashar Fuadi <fushar@gmail.com>
2025-02-12 20:50:41 +0700

    Standardize copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Ikatan Alumni TOKI. Built with Docusaurus.`,
+        copyright: `Copyright © Ikatan Alumni TOKI. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
